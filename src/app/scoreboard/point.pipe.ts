b164735b30a4c3059701e2def09971925d0285f8
vinhanguyen <vinh.a.nguyen@gmail.com>
2022-11-12 20:00:17 -0500

    Display in table

--- src/app/scoreboard/point.pipe.ts
+++ src/app/scoreboard/point.pipe.ts
@@ -7,19 +7,18 @@ const name = ['0', '15', '30', '40'];
 })
 export class PointPipe implements PipeTransform {
 
-  transform(points: number, opponent: number): unknown {
-    try {
-      if (points < 4) {
-        return name[points];
-      } else if (points > opponent) {
-        return 'A';
-      } else if (points === opponent) {
-        return 'D';
-      } else {
-        return '';
-      }
-    } catch (e) {
-      return points;
+  transform(points: number, opponent: number, tiebreak: boolean): unknown {
+    if (tiebreak) {
+      return points
+    };
+    if (points < 4) {
+      return name[points];
+    } else if (points > opponent) {
+      return 'A';
+    } else if (points === opponent) {
+      return 'D';
+    } else {
+      return '';
     }
   }
 
